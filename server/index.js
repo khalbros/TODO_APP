0d5bee71fce0ddd5870359157b63d5a8db9a7121
Muhammad Ghali Umar <muhammadghali63@gmail.com>
2022-01-19 14:17:46 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,13 +12,15 @@ app.use(express.json())
 
 app.use("/todo", todoRoute)
 
-mongoose.connect(process.env.DB_CONNECT).then(
-  () => {
-    console.log("Database is connected")
-  },
-  (err) => {
-    console.log("Can not connect to the database" + err)
-  }
-)
-
-app.listen(port, () => console.log("server running on PORT:" + port))
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(
+    () => {
+      console.log("Database is connected")
+      app.listen(port, () => console.log("server running on PORT:" + port))
+    },
+    (err) => {
+      console.log("Can not connect to the database" + err)
+    }
+  )
+  .catch((err) => console.log(err))
